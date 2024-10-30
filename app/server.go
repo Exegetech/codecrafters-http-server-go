b3afc39324a137682ee95329ba1b0fc07f565b80
Exegetech <swissarmybox@protonmail.ch>
2024-10-29 20:53:22 -0400

    Add 404

--- app/server.go
+++ app/server.go
@@ -1,31 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		log.Fatalln("Failed to bind to port 4221")
 	}
+	defer listener.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln("Error accepting connection: ", err.Error())
+		}
+
+		go handleRequest(conn)
 	}
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	conn.Read(buf)
+
+	req := string(buf)
 
 	msg := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	if !strings.HasPrefix(req, "GET / HTTP/1.1") {
+		msg = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+
 	conn.Write(msg)
+
+	conn.Close()
 }
