ca697116aa11e480ed943208fe3b1e37a68cc7ca
Exegetech <swissarmybox@protonmail.ch>
2024-10-29 21:08:25 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -28,29 +28,17 @@ func main() {
 }
 
 func handleRequest(conn net.Conn) {
-	// buf := make([]byte, 1024)
-	// conn.Read(buf)
-	//
-	// req := string(buf)
-	//
-	// msg := []byte("HTTP/1.1 200 OK\r\n\r\n")
-	// if !strings.HasPrefix(req, "GET / HTTP/1.1") {
-	// 	msg = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
-	// }
-	//
-	// conn.Write(msg)
-	//
-	// conn.Close()
+	buf := make([]byte, 1024)
+	conn.Read(buf)
 
-	req := make([]byte, 1024)
+	req := string(buf)
 
-	conn.Read(req)
-
-	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		conn.Close()
-		return
+	msg := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	if !strings.HasPrefix(req, "GET / HTTP/1.1") {
+		msg = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	conn.Write(msg)
+
+	conn.Close()
 }
